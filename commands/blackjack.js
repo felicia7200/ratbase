3ba9389c25e83ee4a7469cefdd9b8f94bccd019e
Zack D <zsd7200@rit.edu>
2021-10-21 16:10:36 -0400

    blackjack partial code

---
+++ commands/blackjack.js
@@ -0,0 +1,83 @@
+const deckModel = require('../models/deckSchema.js');
+
+// shuffle func modified from https://www.thatsoftwaredude.com/content/6196/coding-a-card-deck-in-javascript
+let shuffle = (deck) => {
+	for(let i = 0; i < 1000; i++) {
+		let loc1 = Math.floor((Math.random() * deck.length));
+		let loc2 = Math.floor((Math.random() * deck.length));
+		let temp = deck[loc1];
+		
+		deck[loc1] = deck[loc2];
+		deck[loc2] = temp;
+	}
+};
+
+module.exports = {
+	name: 'blackjack',
+	aliases: ['bj', 'blackj', 'bjack', '21'],
+	description: 'Play a game of Blackjack and lose all of your $RAT.',
+	cooldown: 10,
+	async execute(message, args, profileData){
+		const gameName = "blackjack";
+		
+		//{ ERROR HANDLING
+		if(!profileData)
+			return message.channel.send("You do not have an account with RATBASE:tm:.");
+		
+		if(!args[0] || args[0].toLowerCase() === "help") {
+			return message.channel.send(
+				"Use this command to gamble $RAT on a game of Blackjack.\n" +
+				"If you win, you get 2x your original bet. If you get 21, " +
+				"then you win 3x your original bet.\n" +
+				"Syntax: **$$blackjack [amount to gamble]**"
+			);
+		}
+		
+		let deckData;
+		try { deckData = await deckModel.findOne({ userID: profileData.userID }); }
+		catch (err) { console.log(err); }
+		
+		// if no deck exists
+		if(!deckData) {
+			let deck = deckModel.create({
+				userID: profileData.userID,
+				game: gameName
+			});
+			
+			try { deckData = await deckModel.findOne({ userID: profileData.userID }); }
+			catch (err) { console.log(err); }
+			
+			deckData.currDeck = shuffle(deckData.fullDeck);
+			deckData.save();
+			
+			//// TODO: STORE TOP CARDS AND DEAL, POP FROM CURRDECK
+		}
+		
+		// check for different game existing
+		if(deckData && (deckData.game !== null || deckData.game !== gameName)) {
+			return message.channel.send(
+				"You are currently in the middle of another card game. " + 
+				"Cannot start aa game of Blackjack."
+			);
+		}
+		
+		// check args[0] for hit/stand/double
+		//// TODO: FILL IN
+		if(deckData && deckData.game === gameName) {
+			switch(args[0]) {
+				case "hit":
+					break;
+				case "stand":
+					break;
+				default:
+					return message.channel.send(
+						"Incorrect argument.\nAcceptible arguments: " +
+						"**hit, stand, double**"
+					);
+					break;
+			}
+		}
+		
+		//// TODO: FUNCTIONS FOR HANDLING WINS/LOSSES
+	}
+}
---
+++ models/deckSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const suits = ["spades", "diamonds", "clubs", "hearts"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let makeCards = () => {
+	let arr = [];
+	
+	for(let i = 0; i < suits.length; i++) {
+		for(let j = 0; j < values.length; j++) {
+			arr.push({value: values[j], suit: suits[i]});
+		}
+	}
+	
+	return arr;
+};
+const deck = makeCards();
+
+const deckSchema = new mongoose.Schema({
+	userID: { type: String, require: true, unique: true },
+	fullDeck: { type: Array, require: true, default: deck },
+	currDeck: { type: Array, require: true, default: deck },
+	game: { type: String, require: true, default: null }
+});
+
+module.exports = model = mongoose.model("Deck", deckSchema);;
