4d5a04866104cb1a2982dfb7b8286dd48340b446
Malcolm-Lambrecht <malcolm.lambrecht@gmail.com>
2021-10-22 15:05:21 -0400

    Yolo time!
    Yolo, 1/7 chance to 4x your balance. On a lose your balance becomes 1. 3/6 of your balance goes to the lowest hodler, 2/6 to the second lowest, and 1/6 to the 3rd lowest.

--- commands/loserboard.js
+++ commands/loserboard.js
@@ -3,19 +3,19 @@ const profileModel = require('../models/profileSchema.js');
 
 module.exports = {
 	name: 'loserboard',
-	description: 'Posts the top 5 worst $RAT hodlers.',
+	description: 'Posts the top 3 worst $RAT hodlers.',
 	aliases: ['losers', 'loser'],
 	cooldown: 30,
 	execute(message, args){
-		profileModel.find({}).sort({ rat: -1 }).exec((err, docs) => {	let index = docs.length - 1;
+		profileModel.find({}).sort({ rat: 1 }).exec((err, docs) => {
 			const embed = new MessageEmbed()
 				.setColor('#0052FE')
 				.setTitle('Worst 3 $RAT Hodlers')
 				.setThumbnail('https://cdn.discordapp.com/attachments/420965765462753283/900974097754914826/ratio.png')
 				.addFields(
-					{ name: `1. ${docs[index].user.split('#')[0]}`, value: `${docs[index].rat} $RAT` },
-					{ name: `2. ${docs[index-1].user.split('#')[0]}`, value: `${docs[index-1].rat} $RAT` },
-					{ name: `3. ${docs[index-2].user.split('#')[0]}`, value: `${docs[index-2].rat} $RAT` }
+					{ name: `1. ${docs[0].user.split('#')[0]}`, value: `${docs[0].rat} $RAT` },
+					{ name: `2. ${docs[1].user.split('#')[0]}`, value: `${docs[1].rat} $RAT` },
+					{ name: `3. ${docs[2].user.split('#')[0]}`, value: `${docs[2].rat} $RAT` }
 				);                                         
 			return message.channel.send({ embeds: [embed] });
 		});
